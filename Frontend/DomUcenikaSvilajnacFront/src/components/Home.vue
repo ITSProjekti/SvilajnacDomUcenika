99cf1554553d9914a04893288b4afbcb1ca7af85
Igor Marjanović <yogimaanz@yahoo.com>
2018-06-20 12:48:41 +0200

    Prva verzija home strane
    jednostavni home

--- Frontend/DomUcenikaSvilajnacFront/src/components/Home.vue
+++ Frontend/DomUcenikaSvilajnacFront/src/components/Home.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>
-        <p>Home Page</p>
-    </div>
+ <v-container>
+     <v-layout row wrap>
+         <v-flex xs12 sm6 class="text-sm-right" absolute> 
+             <v-btn large router to="/ucenici" class="orange darken-4">Prikazi sve ucenike</v-btn>
+         </v-flex>
+         <v-flex xs12 sm6 class="text-sm-left" absolute>
+             <v-btn large router to="/prijava" class="orange darken-4">Prijavi novog ucenika</v-btn>
+         </v-flex>
+     </v-layout>
+ </v-container>
 </template>
