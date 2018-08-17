3f38af357683c9f134f7dc220b5ebd7f15a9aa18
Pavle <pavle.lukic4@gmail.com>
2018-08-17 11:17:15 +0200

    Seedovanje baze, tabele ucenici, tipoviPorodice , roditelji, telefona

--- Backend/DomUcenikaSvilajnac.DAL.Context/Migrations/20180817084133_SlikaUcenikaIOstaliAtributi.cs
+++ Backend/DomUcenikaSvilajnac.DAL.Context/Migrations/20180817084133_SlikaUcenikaIOstaliAtributi.cs
@@ -60,6 +60,38 @@ namespace DomUcenikaSvilajnac.DAL.Context.Migrations
                     table.PrimaryKey("PK_TipoviPorodice", x => x.Id);
                 });
 
+
+            migrationBuilder.Sql("insert into TipoviPorodice (NazivTipaPorodice) values " +
+                          "(N'Kompletna')," +
+                          "(N'Bez majke')," +
+                          "(N'Bez oca')," +
+                          "(N'Bez oba roditelja')," +
+                          "(N'Roditelji razvedeni')");
+
+            migrationBuilder.Sql("insert into BrojeviTelefona (Kucni, Mobilni) values ('01123485', '06x124598')");
+
+
+            migrationBuilder.Sql("Insert into Ucenici (DatumRodjenja, Ime, JMBG, OpstinaId, Prezime, DrzavaRodjenjaId, " +
+                               "OpstinaPrebivalistaId, PolId, TelefonId, Adresa, VremeUpisa, PostanskiBrojId, PrethodnaSkolaId, " +
+                               "UpisanaSkolaId, MestoPrebivalistaId, MestoRodjenjaId, MestoZavrseneSkoleId, SmerId, RazredId, PrethodniUspeh, Slika, TipPorodiceId) " +
+                               "values " +
+                               "('4-4-2010 12:00:00 AM', 'Pera', '1234567890123', 151, 'Perić', 151, 151, 1, (select top 1 Id from BrojeviTelefona)," +
+                               " 'Atlanitda 15', '4-4-2010 12:00:00 AM', 151, 151, 151, 151, 151, 151, 4, 1, 5, 'slika', 1)");
+
+
+
+            migrationBuilder.Sql("insert into Roditelji (Ime, Prezime, UcenikId, BrojTelefona, StepenObrazovanjaId) " +
+                                "values ('ImeO', 'PrezimeO', (select top 1 Id from Ucenici), '06x165584', 8) ," +
+                                "('ImeM', 'PrezimeM', (select top 1 Id from Ucenici), '06x46864', 7)");
+
+
+            
+
+            
+
+            
+
+
             migrationBuilder.CreateIndex(
                 name: "IX_Ucenici_TipPorodiceId",
                 table: "Ucenici",
