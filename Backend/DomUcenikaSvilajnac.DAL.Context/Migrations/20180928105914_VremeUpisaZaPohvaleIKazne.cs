0bc7aae7a2d9fa86298e055e306eed6cd381ca05
Pavle <pavle.lukic4@gmail.com>
2018-09-28 13:18:00 +0200

    Seedovanje tabele StatusiPrijave, dodavanje property-a VremeUpisa
    Propery je dodat u Kazne i Pohvale

---
+++ Backend/DomUcenikaSvilajnac.DAL.Context/Migrations/20180928105914_VremeUpisaZaPohvaleIKazne.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace DomUcenikaSvilajnac.DAL.Context.Migrations
+{
+    public partial class VremeUpisaZaPohvaleIKazne : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "VremeUpisa",
+                table: "Pohvale",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "VremeUpisa",
+                table: "Kazne",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VremeUpisa",
+                table: "Pohvale");
+
+            migrationBuilder.DropColumn(
+                name: "VremeUpisa",
+                table: "Kazne");
+        }
+    }
+}
